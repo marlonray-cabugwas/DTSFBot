673b7b25b469c113b1180d1d29fbb615ceae440d
Marlon Cabugwas <marlonray.cabugwas@sfgov.org>
2018-07-26 13:00:47 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var botbuilder_azure = require("botbuilder-azure");
 var builder_cognitiveservices = require("botbuilder-cognitiveservices");
 var card = require("./cards");
 
+console.log(card.test);
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
